b99033e2f3197d4111a1255c3e1238c4812d1437
Benoit Campeau <benoit@developmen.ca>
2011-11-18 15:33:07 -0500

    removed the splash screen
    Finished the contact page
    added a user admin in active admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,9 +1,16 @@
 class AdminUser < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+  :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  #after_create {|a| a.send_reset_password_instructions }
+
+  def password_required?
+    new_record? ? false : super
+  end
 end
