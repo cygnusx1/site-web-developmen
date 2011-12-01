c5a77264ab78983a865e5d5e9979ad623108012a
Benoit Campeau <benoit@developmen.ca>
2011-12-01 08:48:33 -0500

    fix IE

--- public/javascripts/equipe.js
+++ public/javascripts/equipe.js
@@ -1,7 +1,5 @@
  $(document).ready(function() {
  	
-       $('a[rel=twipsy]').twipsy({'placement': 'below'});
- 		
  		
        $(".info_perso").load( '/equipe/ajax_contact', {name: $(".info_perso").attr("id")}, function(data){ 
        		$(".info_perso").html( data );
