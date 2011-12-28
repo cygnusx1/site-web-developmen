6a2186a2c9bad2aded232a8dcb3bd1253670ac84
Benoit Campeau <benoit@developmen.ca>
2011-12-28 11:20:22 -0500

    I18n du site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
-    puts I18n.locale.to_s
-    puts params
   end
 
   protect_from_forgery
